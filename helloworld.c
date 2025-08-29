7762d8d6052ea4cbb4a82ac475c6c7126ef892f1
Matthew Alexander <matthewaw567@gmail.com>
2025-08-29 18:04:07 +0700

    Testing C

---
+++ helloworld.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(){
+    printf("Hi, my name is Matthew Alexander Widjaja!\n");
+    printf("As a Minecraft enthusiast, my favorite progamming language is Java.");
+    return 0;
+}
